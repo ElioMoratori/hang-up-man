e16a7d8afc48ad21580a23e12539a8adc2ffff9e
ElioMoratori <moratori.elio@gmail.com>
2025-01-09 21:24:47 -0300

    Texto de vitório

--- js/array.js
+++ js/array.js
@@ -1,14 +1,14 @@
-let meiosDeTransporte = ["carro", "avião", "trem", "ônibus", "barco", "bicicleta", "motocicleta", "patinete", "táxi", "metrô", "van", "caminhão", "helicóptero", "dirigível", "canoa", "escuna", "veleiro", "lancha", "submarino", "foguete"]
+let meiosDeTransporte = ["carro", "aviao", "trem", "onibus", "barco", "bicicleta", "motocicleta", "patinete", "taxi", "metro", "van", "caminhao", "helicoptero", "dirigivel", "canoa", "escuna", "veleiro", "lancha", "submarino", "foguete"]
 
-let animais = ["cachorro", "gato", "elefante", "girafa", "leão", "tigre", "macaco", "cobra", "pássaro", "peixe", "baleia", "golfinho", "cavalo", "vaca", "ovelha", "porco", "galinha", "pato", "rato", "coelho", "hiena", "panda", "camelo", "canguru", "morcego", "coruja", "águia", "formiga", "abelha"]
+let animais = ["cachorro", "gato", "elefante", "girafa", "leao", "tigre", "macaco", "cobra", "passaro", "peixe", "baleia", "golfinho", "cavalo", "vaca", "ovelha", "porco", "galinha", "pato", "rato", "coelho", "hiena", "panda", "camelo", "canguru", "morcego", "coruja", "aguia", "formiga", "abelha"]
 
-let comidas = ["pizza", "lasanha", "hambúrguer", "sushi", "macarrão", "arroz", "feijão", "frango", "carne", "peixe", "salada", "sopa", "bolo", "torta", "biscoito", "chocolate", "sorvete", "fruta", "leite", "suco", "café", "chá", "vinho", "cerveja", "sanduíche", "omelete", "panqueca", "pastel"]
+let comidas = ["pizza", "lasanha", "hamburguer", "sushi", "macarrao", "arroz", "feijao", "frango", "carne", "peixe", "salada", "sopa", "bolo", "torta", "biscoito", "chocolate", "sorvete", "fruta", "leite", "suco", "cafe", "cha", "vinho", "cerveja", "sanduiche", "omelete", "panqueca", "pastel"]
 
-let emocoes = ["alegria", "tristeza", "amor", "ódio", "raiva", "medo", "surpresa", "calma", "ansiedade", "felicidade", "inveja", "ciúme", "orgulho", "vergonha", "solidão", "esperança", "desesperança", "satisfação", "frustração", "saudade"]
+let emocoes = ["alegria", "tristeza", "amor", "odio", "raiva", "medo", "surpresa", "calma", "ansiedade", "felicidade", "inveja", "ciume", "orgulho", "vergonha", "solidao", "esperança", "desesperança", "satisfaçao", "frustraçao", "saudade"]
 
-let paises = ["Brasil", "Estados Unidos", "Japão", "China", "Índia", "Rússia", "Alemanha", "França", "Reino Unido", "Itália", "Canadá", "Austrália", "Espanha", "Portugal", "México", "Argentina", "Chile", "Colômbia", "Peru", "Venezuela"]
+let paises = ["Brasil", "Estados Unidos", "Japao", "China", "India", "Russia", "Alemanha", "França", "Reino Unido", "Italia", "Canada", "Australia", "Espanha", "Portugal", "Mexico", "Argentina", "Chile", "Colombia", "Peru", "Venezuela"]
 
-let objetos = ["cadeira", "mesa", "lápis", "caneta", "livro", "caderno", "computador", "celular", "relógio", "chave", "porta", "janela", "bola", "carro", "boneca", "carrinho", "brinquedo", "faca", "garfo", "colher", "copo", "prato", "xícara", "panela", "geladeira", "fogão", "cama", "almofada"];
+let objetos = ["cadeira", "mesa", "lapis", "caneta", "livro", "caderno", "computador", "celular", "relogio", "chave", "porta", "janela", "bola", "carro", "boneca", "carrinho", "brinquedo", "faca", "garfo", "colher", "copo", "prato", "xicara", "panela", "geladeira", "fogao", "cama", "almofada"];
 
 let categoriesName = ["meiosDeTransporte", "animais", "comidas", "emocoes", "paises", "objetos"]
 
